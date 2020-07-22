e6577481e36893ae654b3975a1f917f3677e6da7
abdullahmhd <shk.mhd.abdullah@gmail.com>
2020-07-22 19:55:56 +0530

    Initial commit with index page and index controller

---
+++ src/main/java/guru/sf/recipe/RecipeApplication.java
@@ -0,0 +1,14 @@
+package guru.sf.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeApplication.class, args);
+        System.out.println("test1 e1");
+    }
+
+}
