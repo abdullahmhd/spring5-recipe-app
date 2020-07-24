f49f663cf09272db9e1f36bccdfed9d6a4d6f491
abdullahmhd <shk.mhd.abdullah@gmail.com>
2020-07-24 19:16:02 +0530

    Basic Junit test

---
+++ src/test/java/guru/sf/recipe/domain/CategoryTest.java
@@ -0,0 +1,41 @@
+package guru.sf.recipe.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CategoryTest {
+
+    private Category category;
+
+    private final Long idVal = 4L;
+    private final String descVal = "Test Category";
+    private final Set<Recipe> recipes = new HashSet<>();
+
+    @BeforeEach
+    private void setup() {
+        this.category = new Category();
+        this.category.setId(this.idVal);
+        this.category.setDescription(this.descVal);
+        this.category.setRecipes(this.recipes);
+    }
+
+    @Test
+    void getId() {
+        assertEquals(this.idVal, category.getId());
+    }
+
+    @Test
+    void getDescription() {
+        assertEquals("Wrong Desc", this.category.getDescription());
+    }
+
+    @Test
+    void getRecipes() {
+        assertEquals(this.recipes, this.category.getRecipes());
+    }
+}
